d437c63d59bdeefc64ec4be2ad22de77a98b0af8
Beacon <beacon5122228547@gmail.com>
2026-02-28 02:55:48 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { Home, BookOpen, User, Music, Clipboard } from "lucide-react";
 
-const LOGO = "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69a2266141888b3ccda1983d/5929851bb_sonic1.png";
+const LOGO = "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69a2266141888b3ccda1983d/2cb4b4eb7_sonic2.png";
 
 const PAGE_NAMES = {
   Home: "Home",
